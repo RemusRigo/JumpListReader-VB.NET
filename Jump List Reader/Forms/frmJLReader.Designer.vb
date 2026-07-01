ac6d4b89dcdf623c5822b52e85faadb544a24a54
Remus Rigo <remusrigo@hotmail.com>
2026-07-01 23:59:08 +0300

    2026-07-01 23:59:06

---
+++ Jump List Reader/Forms/frmJLReader.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmJLReader
+   Inherits System.Windows.Forms.Form
+
+   'Form overrides dispose to clean up the component list.
+   <System.Diagnostics.DebuggerNonUserCode()>
+   Protected Overrides Sub Dispose(disposing As Boolean)
+      Try
+         If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+         End If
+      Finally
+         MyBase.Dispose(disposing)
+      End Try
+   End Sub
+
+   'Required by the Windows Form Designer
+   Private components As System.ComponentModel.IContainer
+
+   'NOTE: The following procedure is required by the Windows Form Designer
+   'It can be modified using the Windows Form Designer.
+   'Do not modify it using the code editor.
+   <System.Diagnostics.DebuggerStepThrough()>
+   Private Sub InitializeComponent()
+      lvJLView = New ListView()
+      chkBoxAD = New CheckBox()
+      chkBoxCD = New CheckBox()
+      btnScan = New Button()
+      SuspendLayout()
+      ' 
+      ' lvJLView
+      ' 
+      lvJLView.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+      lvJLView.Location = New Point(1, 26)
+      lvJLView.Name = "lvJLView"
+      lvJLView.Size = New Size(797, 392)
+      lvJLView.TabIndex = 0
+      lvJLView.UseCompatibleStateImageBehavior = False
+      ' 
+      ' chkBoxAD
+      ' 
+      chkBoxAD.AutoSize = True
+      chkBoxAD.Location = New Point(1, 1)
+      chkBoxAD.Name = "chkBoxAD"
+      chkBoxAD.Size = New Size(150, 19)
+      chkBoxAD.TabIndex = 1
+      chkBoxAD.Text = "Automatic Destinations"
+      chkBoxAD.UseVisualStyleBackColor = True
+      ' 
+      ' chkBoxCD
+      ' 
+      chkBoxCD.AutoSize = True
+      chkBoxCD.Location = New Point(160, 1)
+      chkBoxCD.Name = "chkBoxCD"
+      chkBoxCD.Size = New Size(136, 19)
+      chkBoxCD.TabIndex = 2
+      chkBoxCD.Text = "Custom Destinations"
+      chkBoxCD.UseVisualStyleBackColor = True
+      ' 
+      ' btnScan
+      ' 
+      btnScan.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+      btnScan.Location = New Point(756, 427)
+      btnScan.Name = "btnScan"
+      btnScan.Size = New Size(42, 20)
+      btnScan.TabIndex = 3
+      btnScan.Text = "&Scan"
+      btnScan.UseVisualStyleBackColor = True
+      ' 
+      ' frmJLReader
+      ' 
+      AutoScaleDimensions = New SizeF(7F, 15F)
+      AutoScaleMode = AutoScaleMode.Font
+      ClientSize = New Size(800, 450)
+      Controls.Add(btnScan)
+      Controls.Add(chkBoxCD)
+      Controls.Add(chkBoxAD)
+      Controls.Add(lvJLView)
+      Name = "frmJLReader"
+      Text = "Form1"
+      ResumeLayout(False)
+      PerformLayout()
+   End Sub
+
+   Friend WithEvents lvJLView As ListView
+   Protected WithEvents chkBoxAD As CheckBox
+   Protected WithEvents chkBoxCD As CheckBox
+   Friend WithEvents btnScan As Button
+
+End Class
---
+++ Jump List Reader/Forms/frmJLReader.vb
@@ -0,0 +1,83 @@
+﻿Imports System.IO
+Imports System.Runtime
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms.Design.AxImporter
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class frmJLReader
+
+   Private pbLoad As rrProgressBar
+
+   'Dim grp As ListViewGroup = Nothing
+   'Dim log As New Logger(appName)
+
+   Dim pathAD As String = Path.Combine(Environment.GetEnvironmentVariable("appdata"), "Microsoft\Windows\Recent\AutomaticDestinations")
+   Dim pathCD As String = Path.Combine(Environment.GetEnvironmentVariable("appdata"), "Microsoft\Windows\Recent\CustomDestinations")
+
+   Public Function DarkenColor(c As Color, percent As Integer) As Color
+      Dim factor As Double = (100 - percent) / 100.0
+      Return Color.FromArgb(c.A, CInt(c.R * factor), CInt(c.G * factor), CInt(c.B * factor))
+   End Function
+
+   Private Sub frmJLReader_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      lvJLView.View = View.Details
+      lvJLView.FullRowSelect = True
+      lvJLView.Columns.Add("File Name", 250)
+      lvJLView.Columns.Add("Full Path", 500)
+
+      pbLoad = New rrProgressBar()
+      pbLoad.Dock = DockStyle.None
+      pbLoad.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+      pbLoad.Location = New Point(3, Me.ClientSize.Height - pbLoad.Height - 3)
+      pbLoad.Size = New Size(Me.ClientSize.Width - btnScan.Width - 6, 20)
+      'pbLoad.BarColor = DarkenColor(tvOptions.BackColor, 15)
+      'pbLoad.BarColorDone = DarkenColor(tvOptions.BackColor, 30)
+      Me.Controls.Add(pbLoad)
+
+
+   End Sub
+
+   '========================================================================================================================
+   Private Sub btnScan_Click(sender As Object, e As EventArgs) Handles btnScan.Click
+      lvJLView.Items.Clear()
+
+      Dim files As New List(Of String)
+
+      ' AutomaticDestinations ---------------------------------------------------------------------
+      If chkBoxAD.Checked AndAlso Directory.Exists(pathAD) Then
+         'lvJLView.Items.Add(Path.GetFileName(file))
+         files.AddRange(Directory.GetFiles(pathAD, "*.automaticDestinations-ms"))
+      End If
+
+      ' Custom Destinations -----------------------------------------------------------------------
+      If chkBoxCD.Checked AndAlso Directory.Exists(pathCD) Then
+         files.AddRange(Directory.GetFiles(pathCD, "*.customDestinations-ms"))
+      End If
+
+      pbLoad.Maximum = files.Count
+      pbLoad.Value = 0
+      Dim grpAD = New ListViewGroup("Automatic Destinations")
+      lvJLView.Groups.Add(grpAD)
+      Dim grpCD = New ListViewGroup("Custom Destinations")
+      lvJLView.Groups.Add(grpCD)
+
+      For Each f In files
+         Dim name = Path.GetFileName(f)
+         Dim ext As String = Path.GetExtension(f).ToLower()
+
+         Dim item As New ListViewItem(name)
+         item.SubItems.Add(f)
+         item.Tag = f   ' store full path for next steps
+         If ext = ".automaticdestinations-ms" Then
+            item.Group = grpAD
+         ElseIf ext = ".customdestinations-ms" Then
+            item.Group = grpCD
+         End If
+         lvJLView.Items.Add(item)
+
+         pbLoad.Value += 1
+      Next
+      lvJLView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent)
+   End Sub
+
+End Class
